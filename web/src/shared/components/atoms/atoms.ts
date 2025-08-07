510498f077abad732ae91d76f9745b7bef7c308b
Princeton Jude Cipriano <princetonjudecipriano@gmail.com>
2025-08-07 13:41:17 +0800

    feat: add shadcns shared component

---
+++ web/src/shared/components/atoms/atoms.ts
@@ -0,0 +1,7 @@
+export * from './badge/badge.component';
+export * from './button/button.component';
+export * from './input/input.component';
+export * from './label/label.component';
+export * from './progress/progress.component';
+export * from './separator/separator.component';
+export * from './textarea/textarea.component';
